1927707eadd2f7f890d95e42013523d9e63f1f96
cforsythe <cforsythe.cs@gmail.com>
2018-12-02 01:15:18 -0800

    added eureka config

--- src/main/java/freedomphones/phoneservice/data/PhoneDbClient.java
+++ src/main/java/freedomphones/phoneservice/data/PhoneDbClient.java
@@ -10,12 +10,12 @@ import org.springframework.web.client.RestTemplate;
 @Repository
 public class PhoneDbClient {
     public String getPhoneData(){
-        final String uri = "https://freedomphones-zuul-svc.herokuapp.com/phone-service/allPhones";
+        final String uri = "https://freedomphones-zuul-svc.herokuapp.com/phone-db-service/allPhones";
         RestTemplate restTemplate = new RestTemplate();
         return restTemplate.getForObject(uri, String.class);
     }
     public String getPhoneById(String id){
-        final String uri = "https://freedomphones-zuul-svc.herokuapp.com/phone-service/findById/{id}";
+        final String uri = "https://freedomphones-zuul-svc.herokuapp.com/phone-db-service/findById/{id}";
         RestTemplate restTemplate = new RestTemplate();
         Map<String, String> params = new HashMap<String, String>();
         params.put("id", id);
