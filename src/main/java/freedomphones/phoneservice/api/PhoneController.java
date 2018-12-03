dda6101fd89f58f5ab099cf39a10d73ee44b44c7
cforsythe <cforsythe@csumb.edu>
2018-12-02 16:12:44 -0800

    cors

--- src/main/java/freedomphones/phoneservice/api/PhoneController.java
+++ src/main/java/freedomphones/phoneservice/api/PhoneController.java
@@ -16,13 +16,12 @@ public class PhoneController{
     @Autowired
     Manager manager;
 
-    @CrossOrigin
     @GetMapping("/allPhones")
     @ResponseBody
     public String getPhones(){
         return manager.getPhoneList();
     }
-    @CrossOrigin
+    
     @GetMapping("/findById/{id}")
     public String getById(@PathVariable String id){
         return manager.getPhoneById(id);
