39cc167d6b2571875386dd020d139712bbc9903f
cforsythe <cforsythe@csumb.edu>
2018-12-02 17:05:28 -0800

    cors

--- src/main/java/freedomphones/phoneservice/api/PhoneController.java
+++ src/main/java/freedomphones/phoneservice/api/PhoneController.java
@@ -2,7 +2,6 @@ package freedomphones.phoneservice.api;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -10,7 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import freedomphones.phoneservice.business.Manager;
 
-@CrossOrigin(origins = "*", allowedHeaders= "*")
 @RestController
 public class PhoneController{
     @Autowired
